b4a528e889fb4f95b1906dd8392ce02c626c2de9
Gerard Morales <algery_25@icloud.com>
2026-06-25 18:05:30 -0600

    feat(models): crear modelo Mascota

---
+++ app/models/mascota.js
@@ -0,0 +1,8 @@
+export default class Mascota {
+  constructor(nombre, especie, edad, propietario) {
+    this.nombre = nombre;
+    this.especie = especie;
+    this.edad = edad;
+    this.propietario = propietario;
+  }
+}
